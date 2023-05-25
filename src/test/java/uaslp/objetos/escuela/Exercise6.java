251d9163f6808297e810a261a29c553cf15ed237
MikelBarajas38 <mikelbarajas388@gmail.com>
2023-05-25 09:41:44 -0600

    excercise 6 done

--- src/test/java/uaslp/objetos/escuela/Exercise6.java
+++ src/test/java/uaslp/objetos/escuela/Exercise6.java
@@ -2,6 +2,7 @@ package uaslp.objetos.escuela;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Exercise6 {
@@ -25,4 +26,65 @@ public class Exercise6 {
         assertThat(Dependencia2.class).isInterface();
         assertThat(Dependencia3.class).isInterface();
     }
+
+    @Test
+    public void whenAAndBAreEqualThenSaveIsCalled() {
+
+        //given
+        DependenciaStub dependencia = new DependenciaStub();
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia, dependencia, dependencia);
+
+        int a = 8;
+        int b = 8;
+        String c = "test string";
+
+        //when
+        algoritmoX.algoritmoACubrir(a, b, c);
+
+        //then
+        Assertions.assertTrue(dependencia.hasSaved());
+        Assertions.assertTrue(dependencia.hasRecovered());
+
+    }
+
+    @Test
+    public void whenBIsGreaterThanAThenPrintIsCalled() {
+
+        //given
+        DependenciaStub dependencia = new DependenciaStub();
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia, dependencia, dependencia);
+
+        int a = 0;
+        int b = 10;
+        String c = "test string";
+
+        //when
+        algoritmoX.algoritmoACubrir(a, b, c);
+
+        //then
+        Assertions.assertTrue(dependencia.hasPrinted());
+        Assertions.assertTrue(dependencia.hasRecovered());
+
+    }
+
+    @Test
+    public void whenAIsGreaterThanBThenSendIsCalled() {
+
+        //given
+        DependenciaStub dependencia = new DependenciaStub();
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia, dependencia, dependencia);
+
+        int a = 10;
+        int b = 0;
+        String c = "test string";
+
+        //when
+        algoritmoX.algoritmoACubrir(a, b, c);
+
+        //then
+        Assertions.assertTrue(dependencia.hasSent());
+        Assertions.assertTrue(dependencia.hasRecovered());
+
+    }
+
 }
